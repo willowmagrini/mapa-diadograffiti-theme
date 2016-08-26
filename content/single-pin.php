ed3bd3514694c5bab47e60914f77148d429f387c
Matheus Gimenez Petroni <contato@matheusgimenez.com.br>
2016-08-26 13:25:36 -0300

    Update single-pin.php

--- content/single-pin.php
+++ content/single-pin.php
@@ -73,7 +73,8 @@
 										<?php _e( 'Abrir vídeo', 'odin');?>
 									<?php endif;?>
 								</a>
-								<a href="#<?php echo $year;?>" class="col-md-6 pull-left text-center">
+								<?php $link_year = sprintf( '%s.diadograffiti.org', $year );?>
+								<a href="#<?php echo $link_year;?>" class="col-md-6 pull-left text-center">
 									<span class="image-icon text-center">
 										<img src="<?php echo get_template_directory_uri();?>/assets/images/acervo-icon.png" alt="<?php esc_attr_e( 'Acervo Completo', 'odin' );?>" />
 									</span>
